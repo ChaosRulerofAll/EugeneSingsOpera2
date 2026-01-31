65f8fe7f60615456bd68fba26478b464edd384b7
ChaosRulerofAll <ilya@soukoreff.com>
2026-01-31 14:13:47 -0500

    Conductor

---
+++ Assets/Ilya's Noisemakers/ConductorManager.cs
@@ -0,0 +1,55 @@
+using UnityEditor.UI;
+using UnityEngine;
+
+public class ConductorManager : MonoBehaviour
+{
+
+    bool hasHitNote = false;
+    float currentNoteVolume = 0;
+
+    float countDown = 0.0f;
+    float shakeCooficient = 5.0f;
+
+    [SerializeField] Transform shoulder;
+    Transform trans;
+    Transform form;
+    Vector3 shaker;
+
+
+    private void Start()
+    {
+        trans = transform;
+        form = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (countDown < 0)
+        {
+            GetNextNote();
+            print("Volume: " + (currentNoteVolume * 100));
+            countDown = 2.0f;
+
+            shoulder.rotation = Quaternion.Euler(100 * currentNoteVolume - 50, 90, 0);
+        }
+
+        shaker = new Vector3(Mathf.Sin(Time.realtimeSinceStartup * 2 * shakeCooficient),
+            Mathf.Sin(Time.realtimeSinceStartup * 3 * shakeCooficient),
+            Mathf.Sin(Time.realtimeSinceStartup * 7 * shakeCooficient));
+
+        shoulder.localPosition = form.position + (shaker * 0.05f);
+
+        countDown -= Time.deltaTime;
+    }
+
+    void GetNextNote()
+    {
+        currentNoteVolume = Random.Range(0.0f, 1.0f);
+    }
+
+    public void SetShakeCoeficient(float input)
+    {
+        shakeCooficient = input;
+    }
+
+}
